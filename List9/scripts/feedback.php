4592ed79dc5dabee925204e78b5ac61c0c9cd468
JakubSamulski <kubasamulski1@gmail.com>
2022-11-28 20:36:21 +0100

    added php

---
+++ List9/scripts/feedback.php
@@ -0,0 +1,35 @@
+<html>
+    <body>
+        <?php 
+        
+        function validatePhone(){
+            if (preg_match("/[+][\d]{2}\s[\d]{3}[-][\d]{3}[-][\d]{3}/", $_GET["phone"])!=1){
+                header("Location: http://localhost:3000/List9/report.html");
+                die();
+            }
+        }
+
+        validatePhone();
+        echo "hello <br>" . 'we once said "we don\'t want more derailments" but it is what it is' . "<br>";
+        // backslash as escape character
+
+        
+        $name = $_GET["Name"];
+
+        echo "thanks" . $name . "for reporting derailment <br>";
+        $place = $_GET["Place"];
+        $place = strtolower(trim($place));
+
+        if($place=="swojczyce"){
+            echo "tam jeszcze nie ma tramwaju <br>";
+        }
+        else
+        {
+            echo "derailment at " . $place. "<br>";
+        }
+
+        echo "Report recived from " . $_SERVER['REMOTE_ADDR'];
+
+        ?>
+    </body>
+</html>
