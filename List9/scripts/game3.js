4d3c10cbf8dc2f7b40915bde9d5f32d32d3f50a3
Pawel-Kluska <pawel.kluska256@gmail.com>
2022-12-01 12:10:27 +0100

    List9 init

---
+++ List9/scripts/game3.js
@@ -0,0 +1,27 @@
+document.getElementById("startBtn").addEventListener("click", run);
+
+function run() {
+  var randomNumber = Math.floor(Math.random() * 100) + 1;
+  console.log(randomNumber)
+  var nrOfGuesses = 0;
+  while (true) {
+    nrOfGuesses++;
+    var guess = prompt("Guess the number between 1-100");
+    if (guess === null) break;
+    else if (guess == randomNumber) {
+      isNotGuessed = false;
+      alert(
+        "It was " +
+          randomNumber +
+          "\nCongratulations, you guessed the number!\nYou needed " +
+          nrOfGuesses +
+          " guesses"
+      );
+      break;
+    } else if (guess < randomNumber) {
+      alert("Number is higher");
+    } else if (guess > randomNumber) {
+      alert("Number is lower");
+    }
+  }
+}
