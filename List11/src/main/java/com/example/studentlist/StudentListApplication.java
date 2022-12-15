692efedd34f9851c66354897e72eb3b2b9bd0e46
Pawel-Kluska <pawel.kluska256@gmail.com>
2022-12-15 13:37:24 +0100

    Student project template

---
+++ List11/src/main/java/com/example/studentlist/StudentListApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentListApplication.class, args);
+    }
+
+}
